28c96a7737dc61f8f1a737b7530db1ba3cae30d2
GongMeiTing <37677634+GongMeiTing@users.noreply.github.com>
2018-06-14 20:03:26 +0800

    Add files via upload

---
+++ RegistrationController.java
@@ -0,0 +1,53 @@
+package sg.iss.team5cab.contollers;
+
+import java.awt.Component;
+
+import javax.servlet.http.HttpSession;
+import javax.swing.JOptionPane;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import sg.iss.team5cab.model.Users;
+import sg.iss.team5cab.services.UsersService;
+@Controller
+public class RegistrationController {
+	private static final Component frame = null;
+	@Autowired
+    UsersService usersService;
+	
+	@RequestMapping(value="public/register", method = RequestMethod.GET)
+	public ModelAndView displayRegistration(HttpSession session) {
+		return new ModelAndView("user_registration", "Users", new Users());
+	}
+	
+	@RequestMapping(value="public/register", method = RequestMethod.POST)
+	public ModelAndView processRegistration(@ModelAttribute("Users") Users user, HttpSession session) 
+	{
+		
+		System.out.println(user.getAddress());
+		Users authUser = usersService.findUserByUID(user.getUserID());
+		if(authUser!=null) 
+		{
+			user.setPassword("");
+			user.setUserID("");
+			
+			JOptionPane.showMessageDialog(frame,
+				    "the userID has already existed in database");
+			return new ModelAndView("user_registration", "Users", user);
+			
+		}
+		else 
+		{
+			user.setRole("Member");
+			usersService.CreateUser(user);
+			user.setPassword("");
+			ModelAndView mav = new ModelAndView("user_login", "Users", user);
+			return mav;
+		}
+	}
+}
